a0913aff37fbd18812055f2e757cccb5f4ef11a7
Masion-coder <2408766306@qq.com>
2025-03-02 19:30:22 +0800

    JDBC-执行DQL语句

--- src/test/java/com/example/JdbcTest.java
+++ src/test/java/com/example/JdbcTest.java
@@ -2,10 +2,14 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.junit.jupiter.api.Test;
+
+import com.example.module.User;
 
 public class JdbcTest {
 
@@ -29,6 +33,39 @@ public class JdbcTest {
 
                 // 5. 释放资源
             }
+        }
+    }
+
+    @Test
+    public void testSelect() throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://localhost:3306/web01";
+        String user = "root";
+        String password = "123456";
+
+        String query = "SELECT id, username, password, name, age FROM user WHERE username = ? AND password = ?"; // ? 占位符，预编译SQL
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+                PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setString(1, "daqiao");
+            stmt.setString(2, "123456");
+
+            ResultSet rs = stmt.executeQuery(); // 封装查询结果
+
+            while (rs.next()) {
+                User userObj = new User(
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getInt("age"));
+
+                System.out.println(userObj);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
